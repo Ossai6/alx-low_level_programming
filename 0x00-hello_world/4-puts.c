9d7cd1a2887343e78ffd38f4c5affc15d1968aff
Ossai6 <calebossaiakpe@gmail.com>
2022-12-08 09:56:07 -0800

    new

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - A program to print Programming is like building a multilingual puzzle
+ * Return: Always return 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
