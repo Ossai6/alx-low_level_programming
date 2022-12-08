7c795b0f5cfb92fb48c6110b9e6ffb968d34ac85
Ossai6 <calebossaiakpe@gmail.com>
2022-12-08 14:02:04 -0800

    new

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints exactly to the standard error
+ * Return: Always return 1
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 61);
+	return (1);
+}
