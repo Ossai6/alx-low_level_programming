db0efd154644e3836b6c0afa280423b56721756f
Ossai6 <calebossaiakpe@gmail.com>
2022-12-09 11:11:57 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - A program to print single base 10 digits
+ * Return: Always return 0
+ */
+int main(void)
+{
+	int i;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+	i++;
+	}
+	printf("\n");
+	return (0);
+}
