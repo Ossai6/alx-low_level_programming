d01506edb1bffdf7b00e11c8bdda2920243a24f2
Ossai6 <calebossaiakpe@gmail.com>
2022-12-09 18:01:50 -0800

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - A program to print single base 10 digits
+ * Return: Always return 0
+ */
+int main(void)
+{
+	int i;
+	char j;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+	i++;
+	}
+	for (j = 'a'; j < 'g'; j++)
+	{
+		putchar(j);
+	}
+
+	return (0);
+}
