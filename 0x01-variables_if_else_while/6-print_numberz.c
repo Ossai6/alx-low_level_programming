54d8bb1072700f3c7a0d049dee7c2344c1360185
Ossai6 <calebossaiakpe@gmail.com>
2022-12-09 17:40:08 -0800

    rev

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - A program to print single base 10 digits
+ * Return: Always return 0
+ */
+int main(void)
+{
+        int i;
+
+        for (i >= '68' && <= '73');)
+        {
+                putchar(i);
+        }
+  
+        return (0);
+}
