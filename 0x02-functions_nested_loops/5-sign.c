4142bcba0078f055ec5517d30f660260ef401b2d
Ossai6 <calebossaiakpe@gmail.com>
2022-12-13 03:43:27 -0800

    new

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints sign if greater, less or equal 0
+ * @n: Char to be checked
+ * Return: Return 1 if greater than 0, 0 if equal to 0, -1 if less than 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+		_putchar('+');
+	return (1);
+
+	if (n == 0)
+		_putchar('0');
+	return (0);
+
+	else
+		_putchar('-');
+	return (-1);
+}
