abda751209825d98669359a0902c3cd1367aacc7
Ossai6 <calebossaiakpe@gmail.com>
2022-12-19 06:54:59 -0800

    now

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - Swapping to inputs
+ * @a: Integer 1
+ * @b: Integer 2
+ * @x: Variable
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int x;
+
+	*x = *a;
+	*a = *b;
+	*b = s;
+}
