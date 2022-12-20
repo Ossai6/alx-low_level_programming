d14d43f920171caa362067a2a56b25692e22d24e
Ossai6 <calebossaiakpe@gmail.com>
2022-12-19 22:43:28 -0800

    m

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,18 +8,18 @@
 
 void rev_string(char *s)
 {
-	int i = 0;
+	int i = L = 0;
 	int j;
 	char ch;
 
-	while (s[i] != '\0')
+	while (*s[i] != '\0')
 		i++;
-
-	for (j = 0; j <= i; j++)
+	L = i;
+	for (j = i - 1; j <= L; j++)
 	{
 		ch = s[i];
-		s[i] = s[L - 1 - i];
-		s[L - 1 - i] = ch;
+		*s[i] = *s[L - 1 - j];
+		*s[L - 1 - j] = ch;
 	}
-	_putchar(s);
+	_putchar(*s);
 }
