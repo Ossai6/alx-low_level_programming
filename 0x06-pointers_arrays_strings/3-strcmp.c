b22c6e9361cc9e3a931ae31766c07993a5116930
Ossai6 <calebossaiakpe@gmail.com>
2022-12-21 09:40:24 -0800

    new

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,15 +9,16 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, S = 0;
+	int i;
 
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-		if (s1[i] >= s2[i] || s1[i] <= s2[i])
+		if (s1[i] == s2[i])
 		{
-			S = s1[i] - s2[i];
+			return (0);
 		}
-		break;
-	}
-	return (S);
+		else
+		{
+			return (s1[i] - s2[i]);
+		}
 }
