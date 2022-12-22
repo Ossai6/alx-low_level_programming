55b89c84bdad12ad9f2293598aa6a143d852b976
Ossai6 <calebossaiakpe@gmail.com>
2022-12-22 00:07:17 -0800

    updated strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copies a string src to anothe string dest
+ * @dest: Destination string
+ * @src: Source string
+ * @n: Integer
+ * Return: Return dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (i = 0; i < n; i++)
+	{
+		src[i] = '\0';
+	}
+	return (dest);
+}
