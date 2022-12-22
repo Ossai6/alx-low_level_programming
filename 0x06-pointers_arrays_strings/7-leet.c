32a38aa49588ef317cb4d50f2a8f63cee1208c25
Ossai6 <calebossaiakpe@gmail.com>
2022-12-21 20:20:51 -0800

    Wow

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,13 +12,13 @@ char *leet(char *str)
 	char x[] = "aAeEoOtTlL"
 	char y[] = "4433007711"
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; *str[i] != '\0'; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			if (str[i] == x[j];
+			if (*str[i] == x[j];
 			{
-				str[i] = y[j];
+				*str[i] = y[j];
 			}
 		}
 	}
