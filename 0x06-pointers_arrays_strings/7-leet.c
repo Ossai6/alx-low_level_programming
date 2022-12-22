f6cfc10318ca520c8f94721cc0380b63955c22fd
Ossai6 <calebossaiakpe@gmail.com>
2022-12-21 20:27:05 -0800

    Wow

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,18 +9,18 @@
 char *leet(char *str)
 {
 	int i, j;
-	char x[] = "aAeEoOtTlL"
-	char y[] = "4433007711"
+	char x[] = "aAeEoOtTlL";
+	char y[] = "4433007711";
 
-	for (i = 0; *str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			if (*str[i] == x[j];
+			if (str[i] == x[j];
 			{
-				*str[i] = y[j];
+				str[i] = y[j];
 			}
 		}
 	}
-	return (*str);
+	return (str);
 }
