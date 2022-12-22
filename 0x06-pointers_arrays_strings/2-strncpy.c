30983674bc589d08a3555adcb52eb319503295dc
Ossai6 <calebossaiakpe@gmail.com>
2022-12-22 00:40:24 -0800

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,13 +12,13 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; i < n && *(src + i); i++)
 	{
-		dest[i] = src[i];
+		*(dest + i) = *(src + i);
 	}
-	for (i = 0; i < n; i++)
+	for (;i < n; i++)
 	{
-		src[i] = '\0';
+		*(src + i) = '\0';
 	}
 	return (dest);
 }
