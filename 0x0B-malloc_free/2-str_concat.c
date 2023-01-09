5a8bbe66d17d80a596c5627870f3c81f0ac735c3
Ossai6 <calebossaiakpe@gmail.com>
2023-01-09 07:58:22 -0800

    I am on my way becoming a software engineer

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,6 +13,14 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, j = 0, k, m, len1 = 0, len2 = 0;
 	char *p;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 	while (s1[i] != '\0')
 	{
 		len1++;
