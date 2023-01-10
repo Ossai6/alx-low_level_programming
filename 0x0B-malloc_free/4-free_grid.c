9a913e3472563d9c4a5dd2b75396bebe4ab66e7d
Ossai6 <calebossaiakpe@gmail.com>
2023-01-09 16:12:09 -0800

    m

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - a frunction that frees a 2 dimentional grid
+ *@grid: Grid
+ *@height: ingeyer values
+ * Return: Return nothing
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free (grid[i]);
+	}
+	free (grid);
+}
