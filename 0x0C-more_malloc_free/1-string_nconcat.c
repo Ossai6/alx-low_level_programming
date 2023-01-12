6fb61f2f7f2c20f79849e0ffb27b9aa578e3ecc5
Ossai6 <calebossaiakpe@gmail.com>
2023-01-12 03:35:55 -0800

    Last

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	if (n >= len2)
 	{
-		ptr = (char *)malloc((len2 * sizeof(char)) + 1);
+		ptr = malloc(sizeof(char) * (len1 + len2 + 1));
 		if (ptr == NULL)
 			return (NULL);
 	}
