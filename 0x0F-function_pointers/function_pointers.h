5f712e607f8022d2616d7e2496594bc96029a9ea
Ossai6 <calebossaiakpe@gmail.com>
2023-01-19 12:08:46 -0800

    new

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,5 @@
 #ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
-//#include <stddef>	
+#define FUNCTION_POINTERS_H	
 
 char _putchar(char c);
 void print_name(char *name, void (*f)(char *));
